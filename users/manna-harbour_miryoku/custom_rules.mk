6ea89d6a6081a6e2b314abe2f4021db1c37e5360
dliechty <qwertyshoe@gmail.com>
2024-07-13 21:37:48 -0700

    experiment with sofle rgb lighting

--- users/manna-harbour_miryoku/custom_rules.mk
+++ users/manna-harbour_miryoku/custom_rules.mk
@@ -1,8 +1,9 @@
 # Copyright 2019 Manna Harbour
 # https://github.com/manna-harbour/miryoku
 
-RGBLIGHT_SUPPORTED = yes
+#RGBLIGHT_SUPPORTED = yes
 RGB_MATRIX_SUPPORTED = yes
 
 RGBLIGHT_ENABLE = no
 RGB_MATRIX_ENABLE = yes
+RGB_MATRIX_DRIVER = ws2812
