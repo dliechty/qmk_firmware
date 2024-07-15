6e450e73a0ea9afdc4e23f5ed87a521c6f75c5d2
dliechty <qwertyshoe@gmail.com>
2024-07-15 10:30:56 -0700

    adapt 58 key format used on sofle to iris

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -17,30 +17,7 @@
 #define MIRYOKU_LAYER_SYM DLIECHTY_ALTERNATIVES_SYM
 #define MIRYOKU_LAYER_FUN DLIECHTY_ALTERNATIVES_FUN
 
-
-#define WS2812_DI_PIN D3
-
-#define RGBLED_NUM 72
-#define RGBLED_SPLIT { 36, 36 }
-
 #ifdef RGB_MATRIX_ENABLE
-
-#   define RGB_MATRIX_LED_COUNT RGBLED_NUM
-#   define RGB_MATRIX_SPLIT RGBLED_SPLIT
-#   define SPLIT_TRANSPORT_MIRROR
-
-#   define RGB_MATRIX_KEYPRESSES // reacts to keypresses
-// #   define RGB_MATRIX_KEYRELEASES // reacts to keyreleases (instead of keypresses)
-#   define RGB_DISABLE_WHEN_USB_SUSPENDED // turn off effects when suspended
-#   define RGB_MATRIX_FRAMEBUFFER_EFFECTS
-// #   define RGB_MATRIX_LED_PROCESS_LIMIT (RGB_MATRIX_LED_COUNT + 4) / 5 // limits the number of LEDs to process in an animation per task run (increases keyboard responsiveness)
-// #   define RGB_MATRIX_LED_FLUSH_LIMIT 16 // limits in milliseconds how frequently an animation will update the LEDs. 16 (16ms) is equivalent to limiting to 60fps (increases keyboard responsiveness)
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
-
-#    define RGB_MATRIX_HUE_STEP 8
-#    define RGB_MATRIX_SAT_STEP 8
-#    define RGB_MATRIX_VAL_STEP 8
-#    define RGB_MATRIX_SPD_STEP 10
 
 /* Enable the animations you want/need.  You may need to enable only a small number of these because       *
  * they take up a lot of space.  Enable and confirm that you can still successfully compile your firmware. */
@@ -81,7 +58,7 @@
 //#    define ENABLE_RGB_MATRIX_DIGITAL_RAIN
 // enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-//#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE
+#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
 //#    define ENABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
