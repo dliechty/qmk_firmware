2cbaa7c31106f06062076193f7e8eac5f175737d
dliechty <qwertyshoe@gmail.com>
2024-07-15 11:49:23 -0700

    remove unnecessary tab dance code that wasn't being used

--- users/manna-harbour_miryoku/custom_config.h
+++ users/manna-harbour_miryoku/custom_config.h
@@ -19,7 +19,6 @@
 
 #ifdef RGB_MATRIX_ENABLE
 
-#    define RGB_MATRIX_MAXIMUM_BRIGHTNESS 150 // limits maximum brightness of LEDs to 150 out of 255. Higher may cause the controller to crash.
 #    define RGB_MATRIX_HUE_STEP 8
 #    define RGB_MATRIX_SAT_STEP 8
 #    define RGB_MATRIX_VAL_STEP 8
