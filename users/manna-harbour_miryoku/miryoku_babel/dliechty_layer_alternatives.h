9fdbb156fe7b5d3070f10ae33459e1dae0cd0b01
dliechty <qwertyshoe@gmail.com>
2024-10-09 14:13:11 -0700

    add wheel up and down to mouse layer extra buttons

--- users/manna-harbour_miryoku/miryoku_babel/dliechty_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/dliechty_layer_alternatives.h
@@ -45,8 +45,8 @@ U_NP,              U_NP,              U_NP,              U_NA,              U_NA
 #define DLIECHTY_ALTERNATIVES_MOUSE \
 U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              U_NA,            U_NA,         \
 U_NA,              TD(U_TD_BOOT),     TD(U_TD_U_GAME),   TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,           U_NA,         \
-U_NA,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,         U_NA,         \
-U_NA,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,         U_NA,         \
+U_NA,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_MS_L,           KC_MS_D,           KC_MS_U,           KC_MS_R,         KC_WH_U,      \
+U_NA,              KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              U_NU,              KC_WH_L,           KC_WH_D,           KC_WH_U,           KC_WH_R,         KC_WH_D,      \
 U_NP,              U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_BTN2,           KC_BTN1,           KC_BTN3,           U_NP,              U_NP,            U_NP
 
 #define DLIECHTY_ALTERNATIVES_MEDIA \
